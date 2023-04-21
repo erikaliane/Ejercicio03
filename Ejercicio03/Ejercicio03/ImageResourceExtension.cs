7e4f0470e5a43c75e9b42e01be72d325205b656b
ErikaVentura <105476284+erikaliane@users.noreply.github.com>
2023-04-20 22:39:33 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio03/Ejercicio03/ImageResourceExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+using System.Reflection;
+
+namespace Ejercicio03
+{
+
+    [Preserve(AllMembers = true)]
+    [ContentProperty(nameof(Source))]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+                return null;
+            //Do your translation lookup here , using whatever method you
+            var imageSource = ImageSource.FromResource(Source,
+                typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+            return imageSource;
+        }
+
+    }
+}
